cecf442051f75230d5bec5dcbf450d396085797d
alexander Eddie <alexander.eddie@uon.edu.au>
2019-04-24 10:38:19 +0000

    getting it working

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
-
+char text[500];
 int main(){
+    char character;
+    int couter = 0;
+    FILE * inputFile;
+    inputFile = fopen("inputStuff.txt", "r");
+  while (feof(inputFile) == 0 ){
+     
+      fscanf(inputFile, "%c", &character);
+      printf("%c", character);
+      text[couter]= character;
+      
+      couter ++;
+  }
+  for (int i= 0;i<couter;i++){
+      //printf("%c %d", character, i);
+  }
   return 0;
 }
+ 
