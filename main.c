2af6824989855b34ad580816169caf47afdbf9f7
alexander Eddie <alexander.eddie@uon.edu.au>
2019-04-25 11:53:33 +0000

    completed both engip

--- main.c
+++ main.c
@@ -4,11 +4,17 @@
 char text[500];
 char CiphLib[500];
 int numberText[500];
-char alphabet[50];
 char outText[500];
+char  alphabet[2][26] = {{"A""B""C""D""E""F""G""H""I""J""K""L""M""N""O""P""Q""R""S""T""U""V""W""X""Y""Z"},
+     {"a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z"}};
 char character1;
 char character2;
 char grb;
+
+
+//void caps(char * txt, char * abc, int count);
+void rot(int counter, int numOfrotation);
+void subC(int counter2);
 
 int main(){
     
@@ -18,11 +24,11 @@ int main(){
     
     FILE * inputFile;
     FILE * outputF;
-    FILE * CipherLibrary;
+    FILE * cipherLibrary;
     
     
     inputFile = fopen("inputStuff.txt", "r");
-    CipherLibrary = fopen("SubstitutionCipherLibrary.txt","r");
+    cipherLibrary = fopen("SubstitutionCipherLibrary.txt","r");
     outputF = fopen("output.txt", "w");
     
  
@@ -32,43 +38,47 @@ int main(){
         text[counter]= character1;
         counter ++;
     }
-     while (feof(CipherLibrary) == 0){
-        fscanf(CipherLibrary, "%c", &character2);
-        CiphLib[counter2]= character1;
+     while (feof(cipherLibrary) == 0){
+        fscanf(cipherLibrary, "%c", &character2);
+        CiphLib[counter2]= character2;
         counter2++;
     }
   
   fclose (inputFile);
-  fclose (CipherLibrary);
+  fclose (cipherLibrary);
   
-  
-     char  alphabet[2][26] = {{"A""B""C""D""E""F""G""H""I""J""K""L""M""N""O""P""Q""R""S""T""U""V""W""X""Y""Z"},
-     {"a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z"}};
-     // changes lower case to upper case
-     for (int k = 0; k<= counter;k++){
+    for (int k = 0; k<= counter;k++){
          for (int d =0; d<26; d++){
             if (text[k]==alphabet[1][d]){
                 text[k]=alphabet[0][d];
             }
          }
      }
+      for (int k = 0; k<= counter2;k++){
+         for (int d =0; d<26; d++){
+            if (CiphLib[k]==alphabet[1][d]){
+                CiphLib[k]=alphabet[0][d];
+            }
+         }
+     }
      
-     
-     
+     rot(counter, numOfrotation);
+     subC(counter2);
      
      //Substitution Cipher
+     /*
      for (int x = 0; x<=counter2; x++){
          int bool2 = 0;
          for (int z = 0; z<=28; z++){
              if (text[x]==alphabet[0][z]){
                  bool2 = 1;
-                 outText[x]= CiphLib[z];
+                 outText[x] = CiphLib[z];
             }else if((z>=27) && bool2==0){
                 outText[x] = text[x];
             }
         }
     }
-
+*/
      
      
      
@@ -95,10 +105,10 @@ int main(){
    */
    
    
-  for (int i= 0;i<(counter);i++){
+  for (int i= 0;i<=(counter2);i++){
       
-      printf("%c ", outText[i] );
-      fprintf(outputF,"%c", outText[i]);
+      printf("%c ", outText[i]);
+      //fprintf(outputF,"%c", CiphLib[i]);
   }
     //for (int f = 0; f <counter; f++){
       //  fprintf(outputF, "%c", numberText[f]);
@@ -108,21 +118,52 @@ int main(){
   return 0;
 }
 
-/*void rotationCipher(int counter, int rN ){
-     int ct = counter;
-     //int rN = rotNum;
-     char  alphabet[] = {"A""B""C""D""E""F""G""H""I""J""K""L""M""N""O""P""Q""R""S""T""U""V""W""X""Y""Z"};
-     for (int z=0; z<ct; z++){
-        grb = text[z];
-        int num = 0;
-        while ((grb != alphabet[num]) || num < 26){
-            if (num<26){
-                num++;
+void rot(int counter, int numOfrotation){
+       for (int z=0; z<=counter; z++){
+       int bool1 = 0;
+       for(int q = 0; q<=28;q++){
+            if (text[z]==alphabet[0][q]){
+                bool1 = 1;
+                if ((q + numOfrotation)<=25){
+                    numberText[z]=q+numOfrotation;
+                    outText[z]= alphabet[0][q+numOfrotation];
+                }else {
+                    numberText[z]= ((q+numOfrotation)-26);
+                    outText[z]= alphabet[0][(q+numOfrotation)-26];
+                }
+            }else if((q>=27) && bool1==0){
+                outText[z] = text[z];
             }
         }
-        numberText[z] = (num + rN)%25;
+   }
+}
+
+
+//Substitution Cipher
+
+void subC(int counter2){
+     for (int x = 0; x<=counter2; x++){
+         int bool2 = 0;
+         for (int z = 0; z<=28; z++){
+             if (text[x]==alphabet[0][z]){
+                 bool2 = 1;
+                 outText[x] = CiphLib[z];
+            }else if((z>=27) && bool2==0){
+                outText[x] = text[x];
+            }
+        }
     }
-    //for (int f = 0; f <counter; f++){
-     //   printf("%c", numberText[f]);
-    //}
-}*/
+}
+
+/*
+void caps(char * txt, char * abc, int count){
+      for (int k = 0; k<= count;k++){
+         for (int d =0; d<26; d++){
+            if (txt[k]==*abc[1][d]){
+                txt[k]=*abc[0][d];
+            }
+         }
+     }
+}
+*/
+
