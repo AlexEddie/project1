683f32abfba080365fec4c3ad7aa4892c75d6e0d
alexander Eddie <alexander.eddie@uon.edu.au>
2019-04-26 13:59:47 +0000

    still doing stuff

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-//void rotationCipher(int count, int rotNun );
-int numberText[500];
 
+
+int numberText[500];
+char wordLibray[10001][16];
 char text[500];
-//char word[500];
+char word[500][16];
 char CiphLib[30];
 char outText[500];
-char comword[1000];
+
 
 char  alphabet[2][26] = {{"A""B""C""D""E""F""G""H""I""J""K""L""M""N""O""P""Q""R""S""T""U""V""W""X""Y""Z"},
      {"a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z"}};
@@ -15,6 +16,7 @@ char character1;
 char character2;
 char grb;
 
+void rotNK(int counter);
 void print(int counter);
 void caps(int counter, int counter2);
 void rot(int counter, int numOfrotation);
@@ -24,18 +26,38 @@ void subCDN(int counter);
 
 int main(){
     
-    
+    int c1 = 0;
     int counter = 0;
     int counter2 = 0; 
+    
+    char a[16];
+    char b[16];
     //int counter3 = 0;
     
     FILE * inputFile;
     FILE * cipherLibrary;
+    FILE * wordLib;
     
+    wordLib = fopen("wordLibrary.txt", "r");
     inputFile = fopen("inputStuff.txt", "r");
     cipherLibrary = fopen("SubstitutionCipherLibrary.txt","r");
+   
+     for (int n=0; n<500; n++){
+        fscanf(inputFile,"%s", b );
+        for (int m =0; m<16;m++){
+            word[n][m]= b[m];
+        }
+    }
     
-    int c1 = 0;
+  
+   for (int n=0; n<10001; n++){
+        fscanf(wordLib,"%s", a);
+        for (int m =0; m<16;m++){
+            wordLibray[n][m]= a[m];
+        }
+    }
+    
+    fclose(wordLib);
     
     while (feof(cipherLibrary) == 0 ){
         char w;
@@ -51,9 +73,6 @@ int main(){
         counter ++;
     }
     fclose (inputFile);
-    
-   
-    
 
     caps(counter, counter2);
     
@@ -88,7 +107,7 @@ int main(){
                         scanf("%c", &keyYN);
                         if(keyYN==alphabet[0][24]||keyYN==alphabet[1][24]){
                             r=1;
-                            printf("----------------------------------------- \n \nplease ender the decryption key this can be between 1 and 26 \n:");
+                            printf("----------------------------------------- \n \nplease enter the decryption key this can be between 1 and 26 \n:");
                             int nor;
                             scanf("%d", &nor);
                             if ((nor>0)&&(nor<=26)){
@@ -101,7 +120,7 @@ int main(){
                             }   
                         }else if(keyYN==alphabet[0][13]||keyYN==alphabet[1][13]){
                             r=1;
-                            subCDN(counter);
+                            rotNK(counter);
                             print(counter);
                         }   
                     }
@@ -185,6 +204,39 @@ void subCDN(int counter){
 
 
 // rotation cipher 
+void rotNK(int counter){
+    int bool1= 0;
+    //int score=0;
+    char wordArray[26][30];
+    
+    for (int v = 0; v<26 ; v++){
+        
+        for (int j = 0; j<10; j++){
+            
+            for (int d =0; d<28; d++){
+                printf("%c = %c\n", text[j] , alphabet[0][d]);
+                if (text[j] == alphabet[0][d]){
+                    
+                    bool1 = 1;
+                    if (d<=25){
+                        wordArray[v][j] = alphabet[0][d];
+                        
+                    }else {
+                        wordArray[v][j]= alphabet[0][d];
+                        
+                    }
+                }else if((d>=27) && bool1==0){
+                    //printf("hey");
+                    wordArray[v][j] = text[d];
+                }
+            }
+        }
+    }
+    
+    
+   
+   //printf(
+}
 
 void rot(int counter, int numOfrotation){
     for (int z=0; z<=counter; z++){
